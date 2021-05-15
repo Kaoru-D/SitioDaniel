b1116f10158e85f22d1d72bad38f8bf9333a2018
Kaoru-D <57970258+Kaoru-D@users.noreply.github.com>
2021-05-15 17:13:09 -0500

    Cambios de código para crear y actualizar archivo

--- Bussines/DataBase.php
+++ Bussines/DataBase.php
@@ -1,7 +1,8 @@
-<php 
-$_POST["txtDireccionDB"];
-$_POST["txtNombreDB"];
-$_POST["txtUsuario"];
-$_POST["PsContrasenia"];
+<php include_once("../Entidades/Configuracion.php"); include_once("../DataAccess/ConfiguracionData.php"); $_POST["txtDireccionDB"]; $_POST["txtNombreDB"]; $_POST["txtUsuario"]; $_POST["PsContrasenia"]; $objetoClassConfiguracionEntidad=new clConfigEntidad(); $objetoClassConfiguracionEntidad-setearServidor =$_POST["txtDireccionDB"];
+$objetoClassConfiguracionEntidad->setearBaseDatos =$_POST["txtNombreDB"];
+$objetoClassConfiguracionEntidad->setearUsuario =$_POST["txtUsuario"];
+    $objetoClassConfiguracionEntidad->setearClave =$_POST["PsContrasenia"];
 
-?>
+    $objetoClassConfiguracion = new clsConfiguracion();
+    $objetoClassConfiguracion->actualizarArchivo($objetoClassConfiguracionEntidad);
+    ?>

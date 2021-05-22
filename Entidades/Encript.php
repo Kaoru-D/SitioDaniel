2667fbab92d1a5314ed6779ccfd0c4284a2d5395
Kaoru-D <57970258+Kaoru-D@users.noreply.github.com>
2021-05-22 10:10:29 -0500

    crecion de desencriptador

--- Entidades/Encript.php
+++ Entidades/Encript.php
@@ -21,6 +21,15 @@ class clsEncript
         $salida = base64_encode($salida);
         return $salida;
     }
+     public function desencriptar($value)
+     {
+         $salida=false;
+         $llave=hash('sha512',LLAVE);
+         $vector=substr(hash('sha512',VECTOR),0,16);
+         $value=base64_decode($value);
+         $salida=openssl_decrypt($value,METODO,$llave,0,$vector);
+         return $salida;
+     }
 
 
 
@@ -29,4 +38,4 @@ class clsEncript
 
 
 
-1?>
+?>

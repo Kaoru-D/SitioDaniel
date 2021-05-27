8f3d1e86aeffd81bec8df673c8b1364043f51210
Kaoru-D <57970258+Kaoru-D@users.noreply.github.com>
2021-05-27 01:12:22 -0500

    Correciones menores

--- Bussines/DataBase.php
+++ Bussines/DataBase.php
@@ -1,8 +1,29 @@
-<php include_once("../Entidades/Configuracion.php"); include_once("../DataAccess/ConfiguracionData.php"); $_POST["txtDireccionDB"]; $_POST["txtNombreDB"]; $_POST["txtUsuario"]; $_POST["PsContrasenia"]; $objetoClassConfiguracionEntidad=new clConfigEntidad(); $objetoClassConfiguracionEntidad-setearServidor =$_POST["txtDireccionDB"];
-$objetoClassConfiguracionEntidad->setearBaseDatos =$_POST["txtNombreDB"];
-$objetoClassConfiguracionEntidad->setearUsuario =$_POST["txtUsuario"];
-    $objetoClassConfiguracionEntidad->setearClave =$_POST["PsContrasenia"];
+<?php
+include_once('../Entidades/Configuracion.php');
+include_once('../DataAccess/ConfiguracionData.php');
+if(!isset($_POST['txtDireccionDB']))
+{
+    echo 'No Se recuperó el Servidor<br />';
+}
+if(!isset($_POST['txtNombreDB']))
+{
+    echo 'No Se recuperó la base de datos<br />';
+}
+if(!isset($_POST['txtUsuario']))
+{
+    echo 'No Se recuperó el Usuario<br />';
+}
+if(!isset($_POST['PsContrasenia']))
+{
+    echo 'No Se recuperó el password<br />';
+}
 
-    $objetoClassConfiguracion = new clsConfiguracion();
-    $objetoClassConfiguracion->actualizarArchivo($objetoClassConfiguracionEntidad);
-    ?>
+$objClasConfiguracionEntidad = new clConfigEntidad();
+$objClasConfiguracionEntidad->setearServidor($_POST['txtDireccionDB']);
+$objClasConfiguracionEntidad->setearBaseDatos($_POST['txtNombreDB']);
+$objClasConfiguracionEntidad->setearUsuario($_POST['txtUsuario']);
+$objClasConfiguracionEntidad->setearClave($_POST['PsContrasenia']);
+
+$objClsConfiguracion = new clsConfiguracion();
+$objClsConfiguracion->actualizarArchivo($objClasConfiguracionEntidad);
+?>

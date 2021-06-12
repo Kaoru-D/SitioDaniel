2e5b3bd0d8f54224e05f8999048a4be986ba0631
Kaoru-D <57970258+Kaoru-D@users.noreply.github.com>
2021-06-11 22:50:46 -0500

    error en los include_once

--- pages/Home/index.php
+++ pages/Home/index.php
@@ -20,10 +20,10 @@
             <!-- Left navbar links -->
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="../Home/" role="button"><i class="fas fa-bars"></i></a>
+                    <a class="nav-link" data-widget="pushmenu" href="../Home" role="button"><i class="fas fa-bars"></i></a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
-                    <a href="../Home/" class="nav-link">Home</a>
+                    <a href="../Home" class="nav-link">Home</a>
                 </li>
 
             </ul>
@@ -41,7 +41,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="../Home/" class="brand-link">
+            <a href="../Home" class="brand-link">
                 <img src="../../img/logo-eae-blanco-768x189 (1).ico" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Sitio</span>
             </a>
@@ -54,7 +54,7 @@
                         <img src="../../img/usuario.png" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="../Home/" class="d-block">Usuario</a>
+                        <a href="../Home" class="d-block">Usuario</a>
                     </div>
                 </div>
 
@@ -100,16 +100,19 @@
                             if (isset($_GET['Op'])) {
                                 $opcion=$_GET['Op'];
                                 if ($opcion === "RUser") {
-                                    include_once('../Usuarios/Registrar.php');
+                                    include_once('Usuarios/Registrar.php');
                                 }
                                 elseif ($opcion === "RClient") {
-                                    include_once('../Terceros/Registrar.php');
+                                    include_once('Terceros/Registrar.php');
                                 }
                                 elseif ($opcion === "RProvider") {
-                                    include_once('../Terceros/Registrar.php');
+                                    include_once('Terceros/Registrar.php');
                                 }
                                 elseif ($opcion === "RProduct") {
-                                    include_once('../Productos/Registrar.php');
+                                    include_once('Productos/Registrar.php');
+                                }
+                                elseif ($opcion === "CUser") {
+                                    include_once('../Usuarios/consultas.php');
                                 }
                             }
                             ?>
